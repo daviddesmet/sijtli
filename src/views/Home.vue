2e2c375d3a94e7d5785dc2c2164828b4b7aa937a
David De Smet <2607383+idaviddesmet@users.noreply.github.com>
2020-11-07 13:53:39 -0600

    Added some style to home main logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="logo" class="w-64 h-64" src="../assets/img/logo.png" />
+  <div class="min-h-screen w-screen flex items-center">
+    <div class="container mx-auto flex flex-wrap items-center">
+      <div class="w-full md:w-5/12 text-center lg:text-left px-8 md:px-12">
+        <div class="p-12 rounded-lg shadow-md" style="background-color: rgb(160, 159, 208)">
+          <img alt="logo" class="mx-auto w-64 h-64" src="../assets/img/logo.png" />
+          <div class="text-center text-yellow-500">
+            <h2 class="text-lg">Algo....</h2>
+            <div class="text-gray-800 mt-2">Contenido...</div>
+            <div class="text-gray-800 mt-2">Contenido...</div>
+            <div class="text-gray-800 mt-2">Contenido...</div>
+            <div class="text-gray-800 mt-2">Contenido...</div>
+            <div class="text-gray-800 mt-2">Contenido...</div>
+          </div>
+        </div>
+      </div>
+      <div ref="map" class="w-full md:w-7/12 h-full lg:h-screen"></div>
+    </div>
   </div>
-  <div ref="map" class="w-9/12" style="height: 657px"></div>
 </template>
 
 <script lang="ts">
@@ -50,3 +64,9 @@ export default class Home extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#intro-bg {
+  background-color: "#a09fd0";
+}
+</style>
